5f5c97a2896c94943abca59341ca907c16ba7425
jiwoo01 <gwangbok1004@sookmyung.ac.kr>
2020-10-08 15:52:00 +0900

    practice 2

--- main.c
+++ main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 
-void print_star()
+int sumTwo(int a, int b)
 {
-	int i;
-	for(i=0; i<10; i++)
-	   printf("*");
+	return (a+b);
+}
+
+int square(int n)
+{
+	return (n*n);
+}
+
+int get_max(int x, int y)
+{
+	if(x > y)
+	   return(x);
+	else
+	   return(y);
 }
 
 int main(void)
 {
-	print_star();
-	print_star();
-	print_star();
+
+	printf("sumTwo : %i\n", sumTwo(1,2));
+	printf("square : %i\n", square(5));
+	printf("get_max : %i\n", get_max(10,30));
 	
- return 0;	
+	return 0;
 }
 
